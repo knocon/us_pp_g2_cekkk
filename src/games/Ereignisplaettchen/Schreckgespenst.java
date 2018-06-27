62c3fe59766be3ac7e663dfa81f98538c6b68d93
Emre <Emre@LAPTOP-LIS834GM.fritz.box>
2018-06-27 10:13:46 +0200

    Schreckgespenst

--- src/games/Ereignisplaettchen/Schreckgespenst.java
+++ src/games/Ereignisplaettchen/Schreckgespenst.java
@@ -1,5 +1,12 @@
 package games.Ereignisplaettchen;
 
+import games.Zauberberg.Kobold;
+
 public class Schreckgespenst {
 	//TODO Logik des Ereignisses
+    public static void execute (Kobold kobold) {
+	kobold.setLayer(-1);
+	//kobold.setPos(//random Dorf 1..4);
+	
+    }
 }
