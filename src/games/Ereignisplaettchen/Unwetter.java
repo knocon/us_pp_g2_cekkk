24aaef4f066f06e320644167cf78f37f8adaa0c7
Kevin <kevin.fliemann@student.uni-siegen.de>
2018-07-09 18:03:06 +0200

    Logikkommentar

--- src/games/Ereignisplaettchen/Unwetter.java
+++ src/games/Ereignisplaettchen/Unwetter.java
@@ -6,8 +6,20 @@ import games.Zauberberg.*;
 
 public class Unwetter extends Feld {
     
+	private Boolean aufgedeckt;
+	
     public Unwetter(int layer, int feldNr, ArrayList<Kobold> list) {
 	super(layer,feldNr,list);  
     }
-	//TODO Logik des Ereignisses
+	//Keine Javalogik noetig
+    //Wenn aufgedeckt auf True wird, Spielfeld anzeige drehen. Muss nichtmal auf Javaebene runter gehen
+    //Auf Fairplay Basis (wie besprochen) alle Karten selbst umdrehen
+
+	public Boolean getAufgedeckt() {
+		return aufgedeckt;
+	}
+
+	public void setAufgedeckt(Boolean aufgedeckt) {
+		this.aufgedeckt = aufgedeckt;
+	}
 }
