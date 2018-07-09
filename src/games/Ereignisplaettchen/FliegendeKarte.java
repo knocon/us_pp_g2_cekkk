3774c07c7ccbdee9d288f66339b60a04fec57d98
Kevin <kevin.fliemann@student.uni-siegen.de>
2018-07-09 23:25:53 +0200

    Fliegende Karte Randomized Karte

--- src/games/Ereignisplaettchen/FliegendeKarte.java
+++ src/games/Ereignisplaettchen/FliegendeKarte.java
@@ -9,5 +9,15 @@ public class FliegendeKarte extends Feld {
     public FliegendeKarte(int layer, int feldNr, ArrayList<Kobold> list) {
 	super(layer,feldNr,list);
     }
-	//TODO Logik des Ereignisses
+	
+    
+    //TODO Logik des Ereignisses
+    public static void execute (Spieler s, Kartenstapel kartenstapel ) {
+    	int stapelSize = kartenstapel.getStapel().size()-1;
+    	int random = (int )(Math.random() * stapelSize + 0);
+    	s.getHand().add(kartenstapel.getStapel().get(random));
+    	kartenstapel.getStapel().remove(random);
+    }
+
+
 }
