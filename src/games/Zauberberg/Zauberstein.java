4447ebc510b80a5165a02aca90feec74acff1cca
Kevin <kevin.fliemann@student.uni-siegen.de>
2018-07-09 23:38:46 +0200

    Aufgeraeumt

--- src/games/Zauberberg/Zauberstein.java
+++ src/games/Zauberberg/Zauberstein.java
@@ -14,16 +14,12 @@ public class Zauberstein extends Feld {
 	}
 	
 	/*//TODO Richtiges Positionsarray einfuegen
-	Zauberstein zStein0 = new Zauberstein(1, 1, position );
-	Zauberstein zStein1 = new Zauberstein(1, 19, position);
-	Zauberstein zStein2 = new Zauberstein(2, 3, position);
-	Zauberstein zStein3 = new Zauberstein(2, 10, position);
-	Zauberstein zStein4 = new Zauberstein(2, 19, position);
-	Zauberstein zStein5 = new Zauberstein(3, 0, position);
-	Zauberstein zStein6 = new Zauberstein(3, 4, position);
-	Zauberstein zStein7 = new Zauberstein(3, 8, position);
-	//TODO 9. Zauberstein auf Layout
-	//Zauberstein zStein8 = new Zauberstein(feldNr, feldNr, position);
+	 * 
+	 * 
+	
+	
+	
+	
 	*/
 	public Boolean getAufFeld() {
 	    return aufFeld;
