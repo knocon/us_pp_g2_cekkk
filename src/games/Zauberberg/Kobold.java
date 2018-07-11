6ff7b2c12e24c0018c312e1280a62fbb9eded358
Kevin <kevin.fliemann@student.uni-siegen.de>
2018-07-11 22:14:54 +0200

    Begonnen Methoden fuer Bewegung zu schreiben

--- src/games/Zauberberg/Kobold.java
+++ src/games/Zauberberg/Kobold.java
@@ -1,13 +1,16 @@
 package games.Zauberberg;
 
+import java.util.ArrayList;
+
 public class Kobold {
 	private int nummer;
-	private Farbe farbe;
-	//private int [] position = new int [2];  //[layer, position]
+	private Spieler.Name farbe;
 	private int layer; 
-	private int feldNr; 
+	private int feldNr;
+	private int globalFeld; //DER INDEX DES FELDES IN ARRAYLIST felder AUS KLASSE SPIEL
 	private int dorf; 
 	private Spieler spieler; 
+	private Zauberberg zauberberg;
 	
 	//TODO Ereignisplaettchen von Feld erben Lassen
 	//TODO Feldklasse mit Positionsarray, Array mit moeglichen Kobolden, Ereignisplaettchen mit richtigem Ereignis, Zauberstein
@@ -21,16 +24,104 @@ public class Kobold {
 	public Kobold(int nummer, Spieler spieler) {
 	    dorf = (int)(Math.random() * 4+1); //generate random dorf between in a range 1..4
 	    this.layer = -1; 
+	    this.feldNr = -1;
 	    this.spieler = spieler; 
 	    this.nummer = nummer; 
 	}
 	
-
 	
+	//GIBT EIN ARRAY MIT INTWERTEN ZURUECK, WO DER KOBOLD SICH HINBEWEGEN DARF
+	//FORM -> (layer,feld,layer,feld..) 
+	public ArrayList<Integer> moeglFelder(Kobold kobold, int laufweg) {
+		ArrayList<Integer> arrayLayerFeld = new ArrayList<Integer>();
+			if(kobold.getLayer()==-1) {
+	    	    //Schritt raus aus dem Dorf 
+	    	    if(kobold.getDorf()==1) {
+	    	    	arrayLayerFeld.add(0); //layer 
+	    	    	arrayLayerFeld.add((laufweg-1) % 36); //schritte nach "vorne"
+	    	    	arrayLayerFeld.add(0); //layer 
+	    	    	arrayLayerFeld.add(((-(laufweg))-1) % 36); // schritte nach "hinten"
+	    		//arrayLayerFeld.add(1); 
+	    		//arrayLayerFeld.add(Integer.parseInt(dataMap.get("karte2Wert"))-1 % 36); 
+	    		//Fragen wie in dieser Form < <layer, feld>,<layer, feld>,<layer, feld>,....> glaub nicht m?glich ohne vorher anzahl von inneren arrays zu kennen
+	    	    } else if (kobold.getDorf()==2) {
+	    	    	arrayLayerFeld.add(0); //layer 
+	    	    	arrayLayerFeld.add((9+(laufweg)-1) % 36); //schritte nach "vorne"
+	    	    	arrayLayerFeld.add(0); //layer 
+	    	    	arrayLayerFeld.add((9-((laufweg)-1)) % 36); // schritte nach "hinten"
+	    	    } else if (kobold.getDorf() ==3) {
+	    	    	arrayLayerFeld.add(0); //layer 
+	    	    	arrayLayerFeld.add((18+((laufweg)-1)) % 36); //schritte nach "vorne"
+	    	    	arrayLayerFeld.add(0); //layer 
+	    	    	arrayLayerFeld.add((18-((laufweg)-1)) % 36); // schritte nach "hinten"
+	    	    } else if(kobold.getDorf()==4) {
+	    	    	arrayLayerFeld.add(0); //layer 
+	    	    	arrayLayerFeld.add((27+((laufweg)-1)) % 36); //schritte nach "vorne"
+	    	    	arrayLayerFeld.add(0); //layer 
+	    	    	arrayLayerFeld.add((27-((laufweg)-1)) % 36); // schritte nach "hinten"
+	    	    }                	    
+		    }else if(kobold.getLayer()==0) {    
+		    	arrayLayerFeld.add((kobold.getFeldNr()+laufweg) %36);  //vorwärts
+		    	arrayLayerFeld.add((kobold.getFeldNr()-laufweg) %36); // rückwärts
+		    	//arrayLayerFeld.add((((Integer.parseInt(dataMap.get("karte3Wert"))))) % 36); //vorwärts
+		    	//arrayLayerFeld.add((-((Integer.parseInt(dataMap.get("karte3Wert"))))) % 36); // rückwärts
+		    }else if(kobold.getLayer()==1) {                	 
+		    	arrayLayerFeld.add((kobold.getFeldNr()+laufweg) %28); //vorwärts
+		    	arrayLayerFeld.add((kobold.getFeldNr()-laufweg) %28);  // rückwärts
+		    	//arrayLayerFeld.add((((Integer.parseInt(dataMap.get("karte3Wert"))))) % 28); //vorwärts
+		    	//arrayLayerFeld.add((-((Integer.parseInt(dataMap.get("karte3Wert"))))) % 28); // rückwärts
+		   	} else if(kobold.getLayer()==2) {                	 
+	       	    arrayLayerFeld.add((kobold.getFeldNr()+laufweg) %20);  //vorwärts
+	            arrayLayerFeld.add((kobold.getFeldNr()-laufweg) %20); // rückwärts
+	       	    //arrayLayerFeld.add((((Integer.parseInt(dataMap.get("karte3Wert"))))) % 20); //vorwärts
+	       	    //arrayLayerFeld.add((-((Integer.parseInt(dataMap.get("karte3Wert"))))) % 20); // rückwärts
+	       	} else if(kobold.getLayer()==3) {                  	 
+	       	    arrayLayerFeld.add((kobold.getFeldNr()+laufweg) %12);  //vorwärts
+	       	    arrayLayerFeld.add((kobold.getFeldNr()-laufweg) %12);  // rückwärts
+	       	    //arrayLayerFeld.add((((Integer.parseInt(dataMap.get("karte3Wert"))))) % 12); //vorwärts
+	       	    //arrayLayerFeld.add((-((Integer.parseInt(dataMap.get("karte3Wert"))))) % 12); // rückwärts
+	       	}  
+		return arrayLayerFeld;                  
+	}
+	
+	//Idee ist, dass die Methode aufgerufen wird nachdem man weiss, auf welches Feld man will.
+	public void bewegen(Kobold kobold, int tempLayer, int tempFeldNr, Feld feld) {
+		//Bewegung nicht moeglich da einer auf dem Kobold steht	
+		if(feld.getKobolde().get(0)==kobold && feld.getKobolde().size()>1) {
+		//sollte nicht auftreten, da nur Felder 
+		}	
+		//Bewegung moeglich aber kein Layerwechsel
+		else if(feld.getKobolde().size()==1) {
+			kobold.setLayer(tempLayer);
+			kobold.setFeldNr(tempFeldNr);
+		//Bewegung moeglich MIT LAYERWECHSEL
+		}else if(feld.getKobolde().size()>1 && feld.getKobolde().get(1) == kobold) {
+			kobold.setLayer(tempLayer);
+			kobold.setFeldNr(tempFeldNr);			
+		}
+						
+			
+	}
+	
+	public boolean darfBewegen(Kobold kobold, Feld tempFeld) {
+		if(tempFeld.getKobolde().get(0)==kobold && tempFeld.getKobolde().size()>1) {
+			//TODO ANWEISUNG WAS PASSIEREN SOLL, WENN DER KOBOLD SICH NICHT BEWEGEN DARF
+			return false;
+		}else {
+			return true;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	/*
 	public enum Farbe {
 		Schwarz, Rot, Gold, Blau, Gruen
 	}
-	
+	*/ 
 	public void move(int value) {
 	    this.feldNr += value; 
 	}
@@ -84,14 +175,6 @@ public class Kobold {
 
 
 
-	public Farbe getFarbe() {
-		return farbe;
-	}
-
-
-	public void setFarbe(Farbe farbe) {
-		this.farbe = farbe;
-	}
 	
 	public int getDorf() {
 	    return dorf;
@@ -101,6 +184,48 @@ public class Kobold {
 	public void setDorf(int dorf) {
 	    this.dorf = dorf;
 
+	}
+
+
+	public Zauberberg getZauberberg() {
+		return zauberberg;
+	}
+
+
+	public void setZauberberg(Zauberberg zauberberg) {
+		this.zauberberg = zauberberg;
+	}
+
+
+
+
+
+
+
+
+
+	public Spieler.Name getFarbe() {
+		return farbe;
+	}
+
+
+
+
+
+
+
+
+
+	public void setFarbe(Spieler.Name farbe) {
+		this.farbe = farbe;
+	}
+
+	public int getGlobalFeld() {
+		return globalFeld;
+	}
+
+	public void setGlobalFeld(int globalFeld) {
+		this.globalFeld = globalFeld;
 	}
 	
 }
