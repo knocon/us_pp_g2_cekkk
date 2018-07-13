33a9bd5bc70cb5223c233695751c885c80236899
Emre <Emre@LAPTOP-LIS834GM>
2018-07-13 19:03:24 +0200

    fix

--- src/games/Ereignisplaettchen/Fallgrube.java
+++ src/games/Ereignisplaettchen/Fallgrube.java
@@ -11,63 +11,63 @@ public class Fallgrube extends Feld {
 	super(layer,feldNr,list); 
     }
 	
-    public static void execute (Kobold kobold) {
+    public static void execute (Kobold kobold) { 
 	switch(kobold.getLayer()) {
 	case 0: 
-		int random = (int )(Math.random() * 4 + 0);
+	    int random = (int )(Math.random() * 4 + 0);
 	    kobold.setLayer(-1);
 	    kobold.setPos(random);
 	    break;
 	case 1: 
-	    if(kobold.getFeldNr()>0 && kobold.getFeldNr()<7) {
+	    if(kobold.getFeldNr()>0 & kobold.getFeldNr()<7) {
 		kobold.setLayer(0);
 		kobold.setFeldNr(kobold.getFeldNr()+1);
 	    }
-	    if(kobold.getFeldNr()>7 && kobold.getFeldNr()<14) {
+	    if(kobold.getFeldNr()>7 & kobold.getFeldNr()<14) {
 		kobold.setLayer(0);
 		kobold.setFeldNr(kobold.getFeldNr()+3);
 	    }
-	    if(kobold.getFeldNr()>14 && kobold.getFeldNr()<21) {
+	    if(kobold.getFeldNr()>14 & kobold.getFeldNr()<21) {
 		kobold.setLayer(0);
 		kobold.setFeldNr(kobold.getFeldNr()+5);
 	    }
-	    if(kobold.getFeldNr()>21 && kobold.getFeldNr()<28) {
+	    if(kobold.getFeldNr()>21 & kobold.getFeldNr()<28) {
 		kobold.setLayer(0);
 		kobold.setFeldNr(kobold.getFeldNr()+7);
 	    }
 	    break; 
 	case 2: 
-	    if(kobold.getFeldNr()>0 && kobold.getFeldNr()<5) {
+	    if(kobold.getFeldNr()>0 & kobold.getFeldNr()<5) {
 		kobold.setLayer(1);
 		kobold.setFeldNr(kobold.getFeldNr()+1);
 	    }
-	    if(kobold.getFeldNr()>5 && kobold.getFeldNr()<10) {
+	    if(kobold.getFeldNr()>5 & kobold.getFeldNr()<10) {
 		kobold.setLayer(1);
 		kobold.setFeldNr(kobold.getFeldNr()+3);
 	    }
-	    if(kobold.getFeldNr()>10 && kobold.getFeldNr()<15) {
+	    if(kobold.getFeldNr()>10 & kobold.getFeldNr()<15) {
 		kobold.setLayer(1);
 		kobold.setFeldNr(kobold.getFeldNr()+5);
 	    }
-	    if(kobold.getFeldNr()>15 && kobold.getFeldNr()<20) {
+	    if(kobold.getFeldNr()>15 & kobold.getFeldNr()<20) {
 		kobold.setLayer(1);
 		kobold.setFeldNr(kobold.getFeldNr()+7);
 	    }
 	    break; 
 	case 3: 
-	    if(kobold.getFeldNr()>0 && kobold.getFeldNr()<3) {
+	    if(kobold.getFeldNr()>0 & kobold.getFeldNr()<3) {
 		kobold.setLayer(2);
 		kobold.setFeldNr(kobold.getFeldNr()+1);
 	    }
-	    if(kobold.getFeldNr()>3 && kobold.getFeldNr()<6) {
+	    if(kobold.getFeldNr()>3 & kobold.getFeldNr()<6) {
 		kobold.setLayer(2);
 		kobold.setFeldNr(kobold.getFeldNr()+3);
 	    }
-	    if(kobold.getFeldNr()>6 && kobold.getFeldNr()<9) {
+	    if(kobold.getFeldNr()>6 & kobold.getFeldNr()<9) {
 		kobold.setLayer(2);
 		kobold.setFeldNr(kobold.getFeldNr()+5);
 	    }
-	    if(kobold.getFeldNr()>9 && kobold.getFeldNr()<12) {
+	    if(kobold.getFeldNr()>9 & kobold.getFeldNr()<12) {
 		kobold.setLayer(2);
 		kobold.setFeldNr(kobold.getFeldNr()+7);
 	    }
