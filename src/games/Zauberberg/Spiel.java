13064804d280549f8d9970f95a56d2fe14bf60b1
Kevin <kevin.fliemann@student.uni-siegen.de>
2018-07-17 00:16:20 +0200

    Dorf startet bei 0 bis 3

--- src/games/Zauberberg/Spiel.java
+++ src/games/Zauberberg/Spiel.java
@@ -8,6 +8,13 @@ import games.Zauberberg.*;
 public class Spiel {
 
     private Kartenstapel kartenstapel;
+    private Fallgrube[] fallgrube;
+    private FliegendeKarte[] fliegendeKarte;
+    private Geheimgang[] geheimgang;
+    private Kristallkugel[] kristallkugel;
+    private Rabe[] rabe;
+    private Schreckgespenst[] schreckgespenst;
+    private Unwetter[] unwetter;
     private ArrayList<Feld> felder = new ArrayList<Feld>();
     private Zauberberg zauberberg;
 
@@ -15,7 +22,7 @@ public class Spiel {
     public Spiel(Zauberberg zauberberg) {
         this.zauberberg = zauberberg;
         kartenstapel = new Kartenstapel();
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 0; i < 4; i++) {
             felder.add(new Feld(-1, i, this.zauberberg));
         }
 
@@ -111,6 +118,63 @@ public class Spiel {
     public void setFelder(ArrayList<Feld> felder) {
         this.felder = felder;
     }
+
+    public Fallgrube[] getFallgrube() {
+        return fallgrube;
+    }
+
+    public void setFallgrube(Fallgrube[] fallgrube) {
+        this.fallgrube = fallgrube;
+    }
+
+    public FliegendeKarte[] getFliegendeKarte() {
+        return fliegendeKarte;
+    }
+
+    public void setFliegendeKarte(FliegendeKarte[] fliegendeKarte) {
+        this.fliegendeKarte = fliegendeKarte;
+    }
+
+    public Geheimgang[] getGeheimgang() {
+        return geheimgang;
+    }
+
+    public void setGeheimgang(Geheimgang[] geheimgang) {
+        this.geheimgang = geheimgang;
+    }
+
+    public Kristallkugel[] getKristallkugel() {
+        return kristallkugel;
+    }
+
+    public void setKristallkugel(Kristallkugel[] kristallkugel) {
+        this.kristallkugel = kristallkugel;
+    }
+
+    public Rabe[] getRabe() {
+        return rabe;
+    }
+
+    public void setRabe(Rabe[] rabe) {
+        this.rabe = rabe;
+    }
+
+    public Schreckgespenst[] getSchreckgespenst() {
+        return schreckgespenst;
+    }
+
+    public void setSchreckgespenst(Schreckgespenst[] schreckgespenst) {
+        this.schreckgespenst = schreckgespenst;
+    }
+
+    public Unwetter[] getUnwetter() {
+        return unwetter;
+    }
+
+    public void setUnwetter(Unwetter[] unwetter) {
+        this.unwetter = unwetter;
+    }
+
 }
 
 
